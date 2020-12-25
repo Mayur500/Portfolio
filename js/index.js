ad5121ce5932999e88e45d37948540e8b6e58aad
Mayur Goyal <mayurgoyal500@gmail.com>
2020-12-26 03:37:30 +0530

    Initial Commit

---
+++ js/index.js
@@ -0,0 +1,23 @@
+const cv = document.querySelector(".content button");
+const scroll = document.querySelectorAll(".right ul>li");
+const gmail = document.querySelector(".contact .gmail");
+const gmail_image = document.querySelector(".contact .gmail img");
+const gmail_h = document.querySelector(".contact .gmail h3");
+
+cv.addEventListener("click", function (e) {
+  window.open("../CV_Mayur Goyal.pdf", "_blank");
+});
+
+var w = window.innerWidth;
+var h = window.innerHeight;
+scroll.forEach((element, index) => {
+  element.addEventListener("click", (e) => {
+    e.preventDefault();
+    window.scroll(0, index * h);
+  });
+});
+
+gmail_image.addEventListener("mouseover", function (e) {
+  gmail_image.style.transition = "all 2s";
+  gmail_h.style.visibility = "visible";
+});
